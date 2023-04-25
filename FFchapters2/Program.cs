2de242d8e4a4c6c488d1410965dd38719b5de0c9
feuster <alexander.feuster@gmail.com>
2023-04-25 17:53:58 +0200

    Raised version to 2.4
    - Raised version to 2.4 as preparation for the next release

--- FFchapters2/Program.cs
+++ FFchapters2/Program.cs
@@ -36,7 +36,7 @@ bool ChapterStyle1 = false;
 bool ChapterStyle2 = false;
 bool OSLinux = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Linux);
 //GitVersion will be only be actualized/overwritten when using Cake build!
-const string GitVersion = "git-d9cac16";
+const string GitVersion = "git-f0dd47e";
 #endregion
 
 #region Title
